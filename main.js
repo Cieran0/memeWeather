cd9468445a96d9339391c88247709324e888b853
Cieran O'Neill <cieran999@gmail.com>
2022-11-21 22:53:53 +0000

    Added menu

--- main.js
+++ main.js
@@ -1,15 +1,32 @@
 var x;
-var let =0;
+var lat =0;
 var long =0;
 let temp = 0;
 window.onload = function() {
-    x = document.getElementById("demo");
+    //getLocation();
+}
+
+function loadLocation() {
     getLocation();
+}
+
+function readLocation() {
+    lat = document.getElementById("lat").value;
+    long = document.getElementById("long").value;
+}
+
+function run() {
+    document.getElementById("img").alt = "Loading image";
+    readLocation();
+    let address = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"+ lat +"%2C" + long + "?unitGroup=uk&key=GXNC3Q45BZ22QAS9HW2UEFEBC&contentType=json"
+    fetch(address)
+        .then((response) => response.json())
+        .then((data) => doThing(data));
 }
 
 function getLocation() {
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(showPosition);
+      navigator.geolocation.getCurrentPosition(setPosition);
     }
   }
   
@@ -24,16 +41,18 @@ function doThing(data) {
 function doSecondThing(data) {
     console.log(data);
     data = data.features[0].properties;
-    string = data.city + ", " + data.country;
-    document.getElementById("img").src = "http://apimeme.com/meme?meme=Bear-Grylls&top=When+the+weather+is&bottom=+"+temp+"+degrees in " + string;
+    string = ((data.city != undefined)? data.city : data.county) + ", " + data.country;
+    meme = "Bear-Grylls";
+    if(temp < 10) meme = "Bear-Grylls"; 
+    else if(temp < 20) meme = "Arrogant-Rich-Man";
+    else meme = "This-Is-Fine"
+    document.getElementById("img").src = "http://apimeme.com/meme?meme="+meme+"&top=When+the+weather+is&bottom=+"+temp+"+degrees in " + string;
 }
 
-function showPosition(position) {
+function setPosition(position) {
+    console.log(position);
     lat  = position.coords.latitude;
     long = position.coords.longitude;
-    let address = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"+ lat +"%2C" + long + "?unitGroup=uk&key=GXNC3Q45BZ22QAS9HW2UEFEBC&contentType=json"
-    fetch(address)
-        .then((response) => response.json())
-        .then((data) => doThing(data));
-
+    document.getElementById("lat").value = lat;
+    document.getElementById("long").value = long;
 }
